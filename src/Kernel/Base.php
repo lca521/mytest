222fa6003e79fac5f1d52cdb0a0659b6a425f3c7
maven <540458941@qq.com>
2023-07-19 15:48:19 +0800

    2.0

--- src/Kernel/Base.php
+++ src/Kernel/Base.php
@@ -5,37 +5,7 @@ namespace Testmy\Test\Kernel;
 
 class Base
 {
-    protected $config = [];
-    protected $type = null;
-    protected $httpClient = null;
-    protected $accessToken = null;
-
-    protected $extraParams = [];
-
-    private $thoseTypesNeedToCheckFileSystemParams = ['screenshot', 'html2pdf', 'upload'];
-
-    private $fileSystemParams = [
-        'oss' => [
-            'accessId',
-            'accessSecret',
-            'bucket',
-            'endpoint',
-        ],
-        'cos' => [
-            'region',
-            'app_id',
-            'secret_id',
-            'secret_key',
-            'bucket',
-        ],
-        'qiniu' => [
-            'accessKey',
-            'secretKey',
-            'bucket',
-            'domain',
-        ],
-    ];
-
+    
     public function __construct(array $config) {
 
 
